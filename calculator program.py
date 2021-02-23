2ecf43f3708d1bc3eb30c577aeecccacc4ecb862
shubhamchandankar <shubhamchandankar009.com>
2021-02-23 22:01:04 +0530

    program

---
+++ calculator program.py
@@ -0,0 +1,22 @@
+a=int(input("enter the first number : "))
+b=int(input("enter the Second number : "))
+
+def addition(a,b):
+    print(a+b)
+
+addition(a,b)
+
+def sub(a,b):
+    print(a-b)
+
+sub(a,b)
+
+def multiply(a,b):
+    print(a*b)
+
+multiply(a,b)
+
+def divide(a,b):
+    print(a/b)
+
+divide(a,b)
